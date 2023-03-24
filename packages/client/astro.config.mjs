2eb48ed63cc55b46de0516e29d4404a8242258cd
Sai Tarun <sanagapati.tarun@cogoport.com>
2023-03-24 23:45:57 +0530

    fix

--- packages/client/astro.config.mjs
+++ packages/client/astro.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+
+// import dotenv from 'dotenv'
+
+const server = process.env.PUBLIC_SERVER_URL
+console.log("fdgd",server)
+
+// const envPath = `.env.${lang}.local`
+// dotenv.config({ path: envPath })
 
 // https://astro.build/config
 export default defineConfig({
