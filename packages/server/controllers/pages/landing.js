4dd608df6c0be9fa0d070035fa75791e72207e3b
taufeeq-ahmed <taufeeq.ahmed@cogoport.com>
2023-03-29 13:02:21 +0530

    new-data-added-in-db

--- packages/server/controllers/pages/landing.js
+++ packages/server/controllers/pages/landing.js
@@ -13,7 +13,7 @@ const getLandingPageDataFromDB = async (params) => {
         const articles = await getArticlesBySectionIdFromDB(section.section_id);
         if (articles.length == 0) {
             const submissions = await getSubmissionBySectionIdFromDB(section.section_id);
-            section.first_submission_id = submissins[0].submission_id;
+            section.first_submission_id = submissions[0].submission_id;
         } else {
             section.first_article_id = articles[0].article_id;
         }
