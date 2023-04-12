47809f1b87dd523bdb726ffd36a95be6d1b1832e
Shivom10 <shivom.mahar@cogoport.com>
2023-04-12 13:43:48 +0530

    fix: form validation for signup page

--- packages/client/src/components/SignUp/index.jsx
+++ packages/client/src/components/SignUp/index.jsx
@@ -2,20 +2,19 @@ import axios from 'axios';
 import instance from '../../utils/axios';
 import styles from './styles.module.css'
 import eye from '/assets/eye.svg'
+import { useForm } from 'react-hook-form';
 import { useState } from 'react';
 const SignUp = ({ token, data }) => {
     const server = import.meta.env.PUBLIC_SERVER_URL;
-    const [formdata, setFormData] = useState({
-        user_name: '',
-        password: '',
-        github_username: '',
-        email: data
-    });
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = async (formData) => {
+        formData.email = data;
+        // e.preventDefault();
         try {
             const response = await instance.post(`/accept_invite`,
-                { body: { ...formdata, token } },
+                { body: { ...formData, token } },
             );
 
             if (response.status === 200) {
@@ -28,17 +27,19 @@ const SignUp = ({ token, data }) => {
             console.error(error);
         }
     };
-    const handleChange = (e) => {
-        setFormData((prevFormData) => ({
-            ...prevFormData,
-            [e.target.name]: e.target.value
-        }));
-    };
+    const handleShowPassword = (e) => {
+        e.preventDefault();
+        setShowPassword(!showPassword);
+    }
+    const handleShowConfirmPassword = (e) => {
+        e.preventDefault();
+        setShowConfirmPassword(!showConfirmPassword);
+    }
     return (
         <div className={styles.signup_container}>
             <div className={styles.heading}><p>Let's get started for free</p></div>
             <div className={styles.subheading}><p>create your account in one step</p></div>
-            <form className={styles.fm} onSubmit={handleSubmit} id="testForm">
+            <form className={styles.fm} onSubmit={handleSubmit(onSubmit)} id="testForm">
                 <div className={styles.section}>
                     <label htmlFor="full-name">Full Name</label>
                     <input
@@ -46,10 +47,9 @@ const SignUp = ({ token, data }) => {
                         className={styles.text}
                         placeholder="Enter your Full Name"
                         name="user_name"
-                        value={formdata.user_name}
-                        onChange={handleChange}
-                        required
+                        {...register("user_name", { required: true })}
                     />
+                    {errors.user_name && <span className={styles.error}>This field is required</span>}
                 </div>
                 <div className={styles.section}>
                     <label htmlFor="email">Email</label>
@@ -63,34 +63,43 @@ const SignUp = ({ token, data }) => {
                     />
                 </div>
                 <div className={styles.section}>
-                    <label for="password">Password</label>
+                    <label htmlFor="password">Password</label>
                     <div className={styles.password_input}>
                         <input
                             className={styles.password}
-                            type="password"
+                            type={showPassword ? "text" : "password"}
                             id="password"
                             name="password"
-                            onChange={handleChange}
-                            value={formdata.password}
+                            {...register("password", { required: true, minLength: 8, maxLength: 15 })}
                             placeholder="Enter your password"
                         />
-                        <button className={styles.toggle_password}>
+                        <button type="button" className={styles.toggle_password} onClick={handleShowPassword}>
                             <img src={eye} alt="Toggle Password Visibility" />
                         </button>
                     </div>
+                    {errors.password && <span className={styles.error}>Password lenght should be 8-15 letter</span>}
                 </div>
                 <div className={styles.section}>
                     <label htmlFor="confirm-password">Confirm Password</label>
                     <div className={styles.password_input}>
                         <input
                             className={styles.password}
-                            type="password"
+                            type={showConfirmPassword ? "text" : "password"}
+                            {...register("confirm_password", {
+                                required: true,
+                                validate: (value) => value === watch('password')
+                            })}
                             placeholder="Confirm password"
                         />
-                        <button className={styles.toggle_password}>
+                        <button type="button" className={styles.toggle_password} onClick={handleShowConfirmPassword}>
                             <img src={eye} alt="Toggle Password Visibility" />
                         </button>
                     </div>
+                    {errors.confirm_password && errors.confirm_password.type === "required" && (
+                        <span className={styles.error}>This field is required</span>)}
+                    {errors.confirm_password && errors.confirm_password.type === "validate" && (
+                        <span className={styles.error}>Passwords do not match</span>
+                    )}
                 </div>
                 <div className={styles.section}>
                     <label htmlFor="git-username">Git Username</label>
@@ -99,10 +108,10 @@ const SignUp = ({ token, data }) => {
                         className={styles.text}
                         id="git-username"
                         name="github_username"
-                        onChange={handleChange}
-                        value={formdata.github_username}
                         placeholder="Enter your git username"
+                        {...register("github_username", { required: true })}
                     />
+                    {errors.github_username && <span className={styles.error}>This field is required</span>}
                 </div>
                 <div className={styles.btn} >
                     <button id="submit_btn" type="submit">Create Account</button>
