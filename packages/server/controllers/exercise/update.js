08090755fbeb0e27c1ecccc7e7a0b986b74c752b
Shivom10 <shivom.mahar@cogoport.com>
2023-04-25 20:03:56 +0530

    fix: add update api for the exercise

--- packages/server/controllers/exercise/update.js
+++ packages/server/controllers/exercise/update.js
@@ -1,9 +1,15 @@
 const { prisma } = require("../../helpers/db-client");
 const updateExercisesInDB = async (params, data) => {
     const { exercise_id } = params;
-    console.log("----------------------------------------------------")
-    console.log(data);
     const { language, exercise_name, instruction, prefilled_code, test_cases } = data;
+    const exercise = await prisma.exercise.findUnique({ where: { exercise_id } });
+    const test= exercise.test_cases?.map((item)=>{
+        item.test_case_id;
+    })
+    const newTestCases = test_cases
+        .filter((testCase) => testCase.test_case_id === undefined);
+    const existingTestCases=test_cases.filter((tc)=>test?.includes(tc.test_case_id));
+
     const updateExercise = await prisma.exercise.update({
         where: {
             exercise_id: exercise_id,
@@ -14,7 +20,8 @@ const updateExercisesInDB = async (params, data) => {
             prefilled_code: prefilled_code,
             exercise_name: exercise_name,
             test_cases: {
-                create: [...test_cases]
+                create: [...newTestCases],
+                update:existingTestCases,
             }
         }
     })
