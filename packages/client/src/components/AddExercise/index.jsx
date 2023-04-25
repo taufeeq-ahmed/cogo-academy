d54569c379e13ee8f5e47e60b4c088bc48ed9f5e
Sai Tarun <sanagapati.tarun@cogoport.com>
2023-04-25 20:33:38 +0530

    fix:

--- packages/client/src/components/AddExercise/index.jsx
+++ packages/client/src/components/AddExercise/index.jsx
@@ -1,6 +1,6 @@
 import react, { useState } from "react";
-// import EditorComponent from "../CodeEditor";
-// import { RichTextEditor } from '@mantine/rte';
+import EditorComponent from "../CodeEditor";
+import { RichTextEditor } from '@mantine/rte';
 import Dropdown from '../DropDown/Dropdown';
 import InputBox from "../InputBox/InputBox";
 import styles from './styles.module.css'
@@ -8,7 +8,7 @@ import { useForm, useFieldArray, FormProvider, } from 'react-hook-form';
 import instance from '../../utils/axios'
 import Button from '../Button/Button'
 // import JoditEditor from "jodit-react";
-import ControlledRTEditor from "../RichTextEditor/ControlledRTEditor";
+// import ControlledRTEditor from "../RichTextEditor/ControlledRTEditor";
 const languages = [{ label: 'HTML', id: '123' }, { label: 'CSS', id: '124' }, { label: 'JAVASCRIPT', id: '132' }, { label: 'SQL', id: '234' }, { label: 'PYTHON', id: '324' }, { label: 'RUBY', id: '342' }];
 
 const AddExercise = ({ sectionId }) => {
@@ -91,23 +91,23 @@ const AddExercise = ({ sectionId }) => {
                             style_box={{ marginTop: "5px", marginBottom: "5px" }}
                             registerQuery={"instruction"}
                             required /> */}
-                    {/* <RichTextEditor
-                    value={text}
-                    onChange={setText}
-                    register={register}
-                    registerQuery={"instruction"}
-                    placeholder={"instructions"}
-                    required
-                /> */}
+                    <RichTextEditor
+                        value={text}
+                        onChange={setText}
+                        register={register}
+                        registerQuery={"instruction"}
+                        placeholder={"instructions"}
+                        required
+                    />
                     {/* <JoditEditor value={text} onBlur={setText} register={register}
                     registerQuery={"instruction"}
                     placeholder={"instructions"}
                     required /> */}
-                    <ControlledRTEditor id='rte' name='instruction' />
+                    {/* <ControlledRTEditor id='rte' name='instruction' /> */}
                     {/* </div> */}
                     <div className={styles.codeeditor}>
                         <label > Enter Code </label>
-                        {/* <EditorComponent height={'60vh'} register={register} registerQuery={"prefilled_code"} name={"prefilled_code"} onChange={codeHandler} /> */}
+                        <EditorComponent height={'60vh'} register={register} registerQuery={"prefilled_code"} name={"prefilled_code"} onChange={codeHandler} />
                     </div>
                     {fields.map((item, index) => {
                         return (
