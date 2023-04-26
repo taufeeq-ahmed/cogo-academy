dbb54fca9fd6670f9b5a187671d809343fbee3f0
Sai Tarun <sanagapati.tarun@cogoport.com>
2023-04-26 16:27:32 +0530

    fix: logout on admin side

--- packages/server/routes/batch/listWithoutDetails.js
+++ packages/server/routes/batch/listWithoutDetails.js
@@ -5,7 +5,7 @@ const getAllBatchesWithoutDetails = async (fastify) => {
     fastify.get("/batch/list/only-batches", async (req, res) => {
         try {
             const batches = await getAllBatchesWithoutDetailsFromDB();
-            res.status(200).send(batches);
+            res.status(200).send({ user: req.user, batches });
         } catch (err) {
             console.log(err);
         }
