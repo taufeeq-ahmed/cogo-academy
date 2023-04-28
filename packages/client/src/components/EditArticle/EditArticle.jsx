2c3d930f8404154fd73c48ea635f63e1c15eac58
Shivom10 <shivom.mahar@cogoport.com>
2023-04-28 12:34:28 +0530

    fix: add additional link Ui on add article and edit article page and modified its Api

--- packages/client/src/components/EditArticle/EditArticle.jsx
+++ packages/client/src/components/EditArticle/EditArticle.jsx
@@ -1,35 +1,43 @@
 import { useState, useEffect } from 'react';
 import { RichTextEditor } from '@mantine/rte';
-import styles from './styles.module.css'
 import Button from '../Button/Button'
-import { useForm } from 'react-hook-form'
+import { FormProvider, useFieldArray, useForm } from 'react-hook-form'
 import InputBox from '../InputBox/InputBox';
 import instance from '../../utils/axios';
+import './styles.css';
 const EditArticle = ({ article }) => {
 
-    const [text, setText] = useState('');
-    const { article_name, article_time_in_mins, article_content, article_id } = article;
-
-    const { register, handleSubmit } = useForm({
+    // const [text, setText] = useState('');
+    console.log(article);
+    const { article_name, article_time_in_mins, article_content, article_id, Links} = article;
+    const methods = useForm({
         defaultValues: {
             article_name: article_name,
             article_time_in_mins: article_time_in_mins,
+            article_content:article_content,
+            Links: Links
         }
     });
-
-    const onSubmit = async data => { };
-
+    const { register, control, handleSubmit, reset, watch } = methods;
+    const [text, setText] = useState('');
+    const {
+        fields,
+        append,
+        prepend,
+        remove,
+        swap,
+        move,
+        insert,
+        replace
+    } = useFieldArray({
+        control,
+        name: "Links"
+    });
     const updateArticle = async (data) => {
-
+        console.log(data);
         try {
-            await instance.patch(`/article/${article_id}`, {
-                new_data: {
-                    article_name: data.article_name,
-                    article_time_in_mins: data.article_time_in_mins,
-                    article_content: text,
-                    total_score: 1
-                }
-            })
+            data.article_time_in_mins=parseInt(data.article_time_in_mins);
+            await instance.patch(`/article/${article_id}`, data)
             window.location.href='/admin/courses';
         } catch (err) {
 
@@ -39,42 +47,99 @@ const EditArticle = ({ article }) => {
         setText(article_content);
     }, [])
     return (
-        <div className={styles.add_article_container}>
-            <form className={styles.inputs}>
-                <div
-                    className={styles.input_box_container}
-                >
-                    <label htmlFor="">Article Name</label>
-                    <InputBox
-                        register={register}
-                        registerQuery='article_name'
-                        placeholder='Article Name'
+        <FormProvider {...methods}>
+            <form className="edit_article_container" onSubmit={handleSubmit(updateArticle)}>
+                <div className="inputs">
+                    <div className="name_time">
+                        <div
+                            className="input_box_container"
+                        >
+                            <label htmlFor="">Article Name</label>
+                            <InputBox
+                                register={register}
+                                registerQuery='article_name'
+                                placeholder='Article Name'
 
-                    />
+                            />
+                        </div>
+                        <div
+                            className="input_box_container"
+                        >
+                            <label htmlFor="">Time to read</label>
+                            <InputBox
+                                register={register}
+                                registerQuery='article_time_in_mins'
+                                placeholder='Article Time In Minutes'
+                                type='number'
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="">Additional Links</label>
+                        {fields?.map((item, index) => {
+                            return (
+                                <div className="">
+                                    <div className="additional_link">
+                                        <div className="link">
+                                        <label htmlFor="">Link Name</label>
+                                            <InputBox
+                                                placeholder={"Link Name"}
+                                                style={{ fontSize: '16px', marginTop: "5px" }}
+                                                name="link_name"
+                                                register={register}
+                                                style_box={{ marginBottom: "5px" }}
+                                                registerQuery={`Links.${index}.link_name`}
+                                                required />
+                                        </div>
+                                        {/* <img src={cross} alt="delete" /> */}
+                                        <div className='url'>
+                                        <label htmlFor="">Link Url</label>
+                                            <InputBox
+                                                placeholder={"Link_url"}
+                                                style={{ fontSize: '16px', marginTop: "5px" }}
+                                                name="link_url"
+                                                register={register}
+                                                style_box={{ marginBottom: "5px" }}
+                                                registerQuery={`Links.${index}.link_url`}
+                                                required />
+                                        </div>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                        <div className='btn'> 
+                        <div>
+                        <Button
+                            text="+ Add Additional link"
+                            onClick={() => {
+                                append({ link_name: '', link_url: '' });
+                            }}
+                        />
+                        </div>
+                        <div>
+                        <Button text='Reset' onClick={() => reset({
+                            Links: []
+                        })
+
+                        } /></div>
+                        </div>
+                    </div>
                 </div>
-                <div
-                    className={styles.input_box_container}
-                >
-                    <label htmlFor="">Article Name</label>
-                    <InputBox
+                <div className="editor">
+                    <RichTextEditor
+                        value={text}
+                        onChange={setText}
                         register={register}
-                        registerQuery='article_time_in_mins'
-                        placeholder='Article Time In Minutes'
-                        type='number'
+                        registerQuery='article_content'
                     />
+
+                </div>
+                <div className="submit_article">
+                    <Button btnStyle={{ margin: "10px 0" }} text='Save Article' type='submit' />
                 </div>
             </form>
-            <div className={styles.editor}>
-                <RichTextEditor
-                    value={text}
-                    onChange={setText}
-                />
-
-            </div>
-            <div className={styles.submit_article}>
-                <Button btnStyle={{ margin: "10px 0" }} text='Save Article' onClick={handleSubmit(updateArticle)} />
-            </div>
-        </div>
+        </FormProvider>
 
     );
 }
