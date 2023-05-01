2df196853238f8ad332993a6ae007515c9d00532
swapnil-cogoport <swapnil.vaidya@cogoport.com>
2023-05-01 15:23:21 +0530

    fix: adding sql test case

--- packages/client/src/components/CodeEditorLayout/ConsoleTab.jsx
+++ packages/client/src/components/CodeEditorLayout/ConsoleTab.jsx
@@ -1,16 +1,70 @@
 import React, { useRef, useEffect } from 'react';
 import styles from './styles.module.css'
-const ConsoleTab = ({ content , canvas = 'false' }) => {
+function IsJsonString(str) {
+    try {
+      var json = JSON.parse(str);
+      return (typeof json === 'object');
+    } catch (e) {
+      return false;
+    }
+  }
+const ConsoleTab = ({ content , canvas =false, language}) => {
+   
     // alert(JSON.stringify(content))
     const divRef = useRef();
     useEffect(() => {
-        const div = divRef.current;
-        const shadowRoot = div.attachShadow({ mode: 'open' });
-        shadowRoot.innerHTML = (content) ? content : '';
+        if(canvas){
+            const div = divRef.current;
+            const shadowRoot = div.attachShadow({ mode: 'open' });
+            shadowRoot.innerHTML = (content) ? content : '';
+        }
+        
     }, []);
 
     if (canvas) {
+        console.log("inside can");
         return <div ref={divRef} />;
+    }
+    if(language==="SQL"){
+        if(!IsJsonString(content)){
+            return (
+                <div className={styles.console_tab}>
+                    <p>{(content) && content}</p>
+                </div>
+            )
+        }
+        else{
+     
+        const newcontent=JSON.parse(content)
+        console.log("inside sql",newcontent);
+        const head=Object.keys(newcontent[0])
+        console.log(newcontent[0]);
+
+
+        return(
+            <div className={styles.console_tab}>
+            <table>
+            <thead>
+                <tr className={styles.heading_row}>
+                    {head.map((item) => <th>{item}</th>)}
+                </tr>
+            </thead>
+            <tbody>
+                {newcontent?.map((row) => {
+                    const values=Object.values(row)
+                    return (<tr className={styles.student_row}>
+
+                        {values.map((val) => {
+                          
+                           return <td>{val}</td>
+                            
+                        })}
+                    </tr>)
+                })}
+            </tbody>
+        </table>
+        </div>
+        )}
     }
     return (
         <div className={styles.console_tab}>
