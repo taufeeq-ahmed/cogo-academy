0c6032ef4ee68354a6c623012be9e047218d5630
swapnil-cogoport <swapnil.vaidya@cogoport.com>
2023-05-03 10:22:02 +0530

    fix: adding year as int

--- packages/server/migrations/add-sql.js
+++ packages/server/migrations/add-sql.js
@@ -3,106 +3,106 @@ const data = [
     id: 1,
     title: "Toy Story",
     director: "John Lasseter",
-    year: "1995",
-    length_minutes: "81",
+    year: 1995,
+    length_minutes: 81,
   },
   {
     id: 2,
     title: "A Bug's Life",
     director: "John Lasseter",
-    year: "1998",
-    length_minutes: "95",
+    year: 1998,
+    length_minutes: 95,
   },
   {
     id: 3,
     title: "Toy Story 2",
     director: "John Lasseter",
-    year: "1999",
-    length_minutes: "93",
+    year: 1999,
+    length_minutes: 93,
   },
   {
     id: 4,
     title: "Monsters, Inc.",
     director: "Pete Docter",
-    year: "2001",
-    length_minutes: "92",
+    year: 2001,
+    length_minutes: 92,
   },
   {
     id: 5,
     title: "Finding Nemo",
     director: "Andrew Stanton",
-    year: "2003",
-    length_minutes: "107",
+    year: 2003,
+    length_minutes: 107,
   },
   {
     id: 6,
     title: "The Incredibles",
     director: "Brad Bird",
-    year: "2004",
-    length_minutes: "116",
+    year: 2004,
+    length_minutes: 116,
   },
   {
     id: 7,
     title: "Cars",
     director: "John Lasseter",
-    year: "2006",
-    length_minutes: "117",
+    year: 2006,
+    length_minutes: 117,
   },
   {
     id: 8,
     title: "Ratatouille",
     director: "Brad Bird",
-    year: "2007",
-    length_minutes: "115",
+    year: 2007,
+    length_minutes: 115,
   },
   {
     id: 9,
     title: "WALL-E",
     director: "Andrew Stanton",
-    year: "2008",
-    length_minutes: "104",
+    year: 2008,
+    length_minutes: 104,
   },
   {
     id: 10,
     title: "Up",
     director: "Pete Docter",
-    year: "2009",
-    length_minutes: "101",
+    year: 2009,
+    length_minutes: 101,
   },
   {
     id: 11,
     title: "Toy Story 3",
     director: "Lee Unkrich",
-    year: "2010",
-    length_minutes: "103",
+    year: 2010,
+    length_minutes: 103,
   },
   {
     id: 12,
     title: "Cars 2",
     director: "John Lasseter",
-    year: "2011",
-    length_minutes: "120",
+    year: 2011,
+    length_minutes: 120,
   },
   {
     id: 13,
     title: "Brave",
     director: "Brenda Chapman",
-    year: "2012",
-    length_minutes: "102",
+    year: 2012,
+    length_minutes: 102,
   },
   {
     id: 14,
     title: "Monsters University",
     director: "Dan Scanlon",
-    year: "2013",
-    length_minutes: "110",
+    year: 2013,
+    length_minutes: 110,
   },
   {
     id: 15,
     title: "Toy Story 4",
     director: "John Lasseter",
-    year: "2017",
-    length_minutes: "117",
+    year: 2017,
+    length_minutes: 117,
   },
 ];
 
@@ -54586,16 +54586,16 @@ function main() {
     });
   });
 
-  data1.map(async (k1) => {
-    adminUser = await prisma.employees.create({
-      data: k1,
-    });
-  });
-  data3.map(async (k3) => {
-    adminUser = await prisma.patients.create({
-      data: k3,
-    });
-  });
+  // data1.map(async (k1) => {
+  //   adminUser = await prisma.employees.create({
+  //     data: k1,
+  //   });
+  // });
+  // data3.map(async (k3) => {
+  //   adminUser = await prisma.patients.create({
+  //     data: k3,
+  //   });
+  // });
 
   console.log("table added successfully");
 }
