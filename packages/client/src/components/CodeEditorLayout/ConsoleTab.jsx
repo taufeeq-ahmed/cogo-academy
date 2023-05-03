b901f5e79d628793d9dd018295c5182f9f80b714
swapnil-cogoport <swapnil.vaidya@cogoport.com>
2023-05-03 11:57:07 +0530

    fix: removing condition to normal

--- packages/client/src/components/CodeEditorLayout/ConsoleTab.jsx
+++ packages/client/src/components/CodeEditorLayout/ConsoleTab.jsx
@@ -1,13 +1,6 @@
 import React, { useRef, useEffect } from 'react';
 import styles from './styles.module.css'
-function IsJsonString(str) {
-    try {
-      var json = JSON.parse(str);
-      return (typeof json === 'object');
-    } catch (e) {
-      return false;
-    }
-  }
+
 const ConsoleTab = ({ content , canvas =false, language}) => {
    
     // alert(JSON.stringify(content))
@@ -24,44 +17,44 @@ const ConsoleTab = ({ content , canvas =false, language}) => {
     if (canvas) {
         return <div ref={divRef} />;
     }
-    if(language==="sql"){
+    // if(language==="sql"){
        
-            return (
-                <div className={styles.console_tab}>
-                    <p>{(content) && content}</p>
-                </div>
-            )
+    //         return (
+    //             <div className={styles.console_tab}>
+    //                 <p>{(content) && content}</p>
+    //             </div>
+    //         )
         
-        // else{
+    //     // else{
      
-        // const newcontent=JSON.parse(content)
-        // const head=Object.keys(newcontent[0])
+    //     // const newcontent=JSON.parse(content)
+    //     // const head=Object.keys(newcontent[0])
 
-        // return(
-        //     <div className={styles.console_tab}>
-        //     <table>
-        //     <thead>
-        //         <tr className={styles.heading_row}>
-        //             {head.map((item) => <th>{item}</th>)}
-        //         </tr>
-        //     </thead>
-        //     <tbody>
-        //         {newcontent?.map((row) => {
-        //             const values=Object.values(row)
-        //             return (<tr className={styles.student_row}>
+    //     // return(
+    //     //     <div className={styles.console_tab}>
+    //     //     <table>
+    //     //     <thead>
+    //     //         <tr className={styles.heading_row}>
+    //     //             {head.map((item) => <th>{item}</th>)}
+    //     //         </tr>
+    //     //     </thead>
+    //     //     <tbody>
+    //     //         {newcontent?.map((row) => {
+    //     //             const values=Object.values(row)
+    //     //             return (<tr className={styles.student_row}>
 
-        //                 {values.map((val) => {
+    //     //                 {values.map((val) => {
                           
-        //                    return <td>{val}</td>
+    //     //                    return <td>{val}</td>
                             
-        //                 })}
-        //             </tr>)
-        //         })}
-        //     </tbody>
-        // </table>
-        // </div>
-        // )}
-    }
+    //     //                 })}
+    //     //             </tr>)
+    //     //         })}
+    //     //     </tbody>
+    //     // </table>
+    //     // </div>
+    //     // )}
+    // }
     return (
         <div className={styles.console_tab}>
             <p>{(content) && content}</p>
